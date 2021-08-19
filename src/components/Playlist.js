3b33dfee82fc07a5f457e502ef1f3d32a206f85c
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-19 11:43:14 -0400

    created song component

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import Song from '../components/Song'
 
 const Playlist = (props) => {
     const loaded = () => {
         return (
         <div>
             <h1> Playlist 1 </h1> 
+            <h3>For all your playlist needs!</h3>
                 <div className="songContainer">
-                    {/* <Song/> */}
+                    <Song/>
                 </div>
         </div>
         )
     }
     const loading = () => {
-        return <div>Items loading...</div>
+        return <div>Playlist loading...</div>
     }
     return props ? loaded() : loading()
 }
