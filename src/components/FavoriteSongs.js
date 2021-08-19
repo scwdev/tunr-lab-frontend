797c7613203f271e6d965c2e69ecf5be9ee9d4ec
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-08-19 12:12:58 -0400

    favorite songs component

---
+++ src/components/FavoriteSongs.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Song from '../components/Song'
+
+const FavoriteSongs = (props) => {
+    const loaded = () => {
+        return (
+        <div>
+            <h1> Favorite Songs List </h1> 
+                <div className="favoriteSongsContainer">
+                    <Song/>
+                </div>
+        </div>
+        )
+    }
+    const loading = () => {
+        return <div>Favorite Songs loading...</div>
+    }
+    return props ? loaded() : loading()
+}
+
+export default FavoriteSongs
